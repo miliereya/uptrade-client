d08aedd899461db89d787f254e7dc254e00842ee
miliereya <105806526+miliereya@users.noreply.github.com>
2022-12-04 20:21:32 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const languages = ['ru', 'en']
 
-export const server_api = 'https://uptrade-to-do-list.netlify.app/api/'
+export const server_api = 'https://uptrade-to-do-list-server.netlify.app/api/'
