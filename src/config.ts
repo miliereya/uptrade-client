73bfceb0f5465b0c8405ef09a7cbc281b164fa28
miliereya <105806526+miliereya@users.noreply.github.com>
2022-12-04 21:57:42 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const languages = ['ru', 'en']
 
-export const server_api = 'https://uptrade-to-do-list-server.netlify.app/api/'
+export const server_api = 'https://uptrade-tdl-server.herokuapp.com/api/'
